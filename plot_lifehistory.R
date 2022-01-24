98a3307ffc98bc5d95785a37315f6544b138558a
jamesmaino <jamesmaino@gmail.com>
2022-01-24 16:15:14 +1100

    pasture data

--- plot_lifehistory.R
+++ plot_lifehistory.R
@@ -1,32 +1,13 @@
 library(tidyverse)
 library(lubridate)
 
-# mite eggs
-d1 = "data/lifehistory/egg development_ ngn_keys_1990_1992 20_Dec.xlsx" %>%
-  readxl::read_xlsx(sheet=1)
-d1$SITE %>% unique
-
-d1 %>% 
-  mutate(DATE = as.Date(DATE)) %>%
-  mutate(diapause_prop = AESTIV./(WINTER + AESTIV.)) %>% 
-  mutate(diapause = diapause_prop > 0.5) %>%
-  group_by(SITE, YEAR, DATE) %>% 
-  summarise(mean_diapause = mean(diapause) ,groups="drop") %>% 
-  ggplot(aes(DATE, mean_diapause)) + 
-  geom_point() + 
-  geom_line() +
-  scale_x_date(date_labels = "%b %Y") +
-  facet_wrap( ~ SITE, scales = "free_x") 
-ggsave("plots/lifehistoryegg.png", width = 10)
-
-
 # mite life history
-d2 = "data/mite population_ ngn_keys_1990-1992 20_Dec.xlsx" %>%
+d1 = "data/mite population_ ngn_keys_1990-1992 20_Dec.xlsx" %>%
   readxl::read_xlsx(sheet=1)
-names(d2)
+names(d1)
 lifestagenames = c("LARVA", "PROTO", "DEUTO", "TRITO", "ADULT FEMALE", 
                    "ADULT MALE", "TOTAL ADULTS", "EGGS ON PASTURE")
-d2 %>% 
+d1 %>% 
   mutate(DATE = as.Date(DATE)) %>%
   mutate(DATE2 = as.Date(format(DATE, "1990-%m-%d"))) %>%
   pivot_longer(cols = lifestagenames, names_to = "stage", values_to = "number") %>% 
@@ -43,12 +24,52 @@ d2 %>%
   ylab("Abundance")
 ggsave("plots/lifehistorymite.png", width = 10, height=7)
 
-d = d2 %>% 
-  drop_na(`EGGS ON PASTURE`) 
 
-ggplot(d) +
-  geom_point(aes(`ADULT FEMALE`, `EGGS ON PASTURE`)) + 
-  scale_x_log10() + 
-  scale_y_log10()
+# mite eggs
+d2 = "data/lifehistory/egg development_ ngn_keys_1990_1992 20_Dec.xlsx" %>%
+  readxl::read_xlsx(sheet=1)
+d2$SITE %>% unique
 
-lm(data=d)
+d2 %>% 
+  mutate(DATE = as.Date(DATE)) %>%
+  mutate(diapause_prop = AESTIV./(WINTER + AESTIV.)) %>% 
+  mutate(diapause = diapause_prop > 0.5) %>%
+  group_by(SITE, YEAR, DATE) %>% 
+  summarise(mean_diapause = mean(diapause) ,groups="drop") %>% 
+  ggplot(aes(DATE, mean_diapause)) + 
+  geom_point() + 
+  geom_line() +
+  scale_x_date(date_labels = "%b %Y") +
+  facet_wrap( ~ SITE, scales = "free_x") 
+ggsave("plots/lifehistoryegg.png", width = 10)
+
+
+# over summering eggs
+d3 = "data/summer sample_ngn_keys_1990-1992 17-Jan.xlsx" %>%
+  readxl::read_xlsx(sheet=1) %>% 
+  mutate(`No. EGGS` = as.numeric(`No. EGGS`) ) %>% 
+  mutate(DATE= as.Date(DATE))
+
+dsum = d3 %>% 
+  group_by(SITE, SAMPLE, DATE) %>% 
+  summarise(nbodies = sum(`BODIES/CORE`,na.rm=T),
+            nbodiesdissected = n(), 
+            total_eggs = sum(`No. EGGS`, na.rm=T),
+            mean_eggs_per_mite = mean(`No. EGGS`, na.rm=T), 
+            se_eggs = sd(`No. EGGS`)/sqrt(nbodiesdissected),
+            .groups="drop") %>% 
+  mutate(nbodiesdissected = if_else(nbodies == 0, as.integer(0), nbodiesdissected))
+
+dsum %>% 
+  pivot_longer(cols = c(nbodies, nbodiesdissected, total_eggs, mean_eggs_per_mite, se_eggs)) %>% 
+  filter(name %in% c("nbodies", "mean_eggs_per_mite")) %>%
+  filter(value < 400) %>% # !!! filter outlier for plotting
+  mutate(Year = factor(year(DATE))) %>%
+  ggplot(aes(Year, value, group=DATE)) +
+  geom_violin() +
+  # scale_y_log10() +
+  facet_grid(name~SITE, switch = "y", scales="free_y")
+
+ggsave("plots/lifehistoryegg_summer.png", width = 10, height=7)
+
+
---
+++ plot_pasture_height.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(lubridate)
+
+# effect of pasture height
+col_types = 
+  c("date","numeric","text", rep("numeric", 34), 
+   "text", "text", "numeric", "numeric", "text")  
+
+d = "data/Short Tall mites and pasture 17-Jan.xls" %>%
+  readxl::read_xls(sheet=1, 
+                   col_types = col_types) %>% 
+  mutate(DATE= as.Date(DATE)) %>%
+  mutate(CLOV_CM_SHORT = as.numeric(CLOV_CM_SHORT)) %>%
+  mutate(CLOV_CM_TALL = as.numeric(CLOV_CM_TALL)) %>%
+  pivot_longer(cols = -c(DATE, WEEK, SAMPLE, YEAR,	
+                         State,	SITE,	Latitude,	Longitude,	COLLECTOR)) %>% 
+  drop_na(value) %>% 
+  mutate(name = gsub("TALL", "_TALL", name)) %>%
+  mutate(name = gsub("SHORT", "_SHORT", name)) %>%
+  separate(col = name, into=c("var", "pasture"), sep = "__")
+
+names(d)
+
+dsum = d %>% 
+  group_by(SITE, DATE, WEEK, pasture, var) %>% 
+  summarise(
+    mean_val = mean(value), 
+    se_val = sd(value)/sqrt(n()),
+    .groups="drop") 
+
+unique(d$var)
+dsum %>% 
+  ggplot(aes(DATE, mean_val, col=pasture)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=mean_val-se_val, ymax=mean_val+se_val, width=1)) + 
+  geom_line() +
+  facet_wrap(~var, scales = "free_y")
+
+ggsave("plots/pasture_height_effect.png", width = 4, height=3, scale = 2)  
